6c33867b05c67b385bdc7a8e3556505798494435
Tobiasz Luszowski <0tobik09@gmail.com>
2021-07-02 17:05:37 +0200

    💄 Perfecto

--- main.py
+++ main.py
@@ -13,14 +13,30 @@ class Main:
     def __init__(self):
         self.plugin_folder_path: str = os.path.join(os.getcwd(), "plugins")
         self.loaded_plugins: list = []
+        self.dictionary: dict = {
+            "on_load": "DevTools is on fire!!!",
+            "on_unload": "Goodbye my friend :)",
+            "load_plugin": "Loading {} plugin",
+            "unload_plugin": "Unloading {}...",
+            "wrong_api": "Plugin {} can't be loaded due to bad plugin API version.",
+            "wrong_api2": "Plugin API version {}",
+            "wrong_api3": "Required API version {}",
+            "loaded_x_plugins": "Loaded {} plugins",
+            "success_loading": "Successfull loaded {}"
+        }
         
+    def liblary(self, short: str, *args):
+        if short in self.dictionary:
+            self.info(self.dictionary[short].format(args[0]) if len(args) != 0
+                      else self.dictionary[short])
+    
     def on_load(self):
         self.logger: logger = self.server.logger
-        self.logger.info("DevTools is on fire!!!")
+        self.logger.info(self.liblary("on_load"))
         self.getAllFolders()
 
     def on_unload(self):
-        self.logger.notice("Goodbye my friend :)")
+        self.logger.notice(self.liblary("on_unload"))
 
     def toxic_load(self,path:str) -> int:
         plugin_info: dict = json.load(open(os.path.join(path, "info.json"), 'r'))
@@ -29,20 +45,20 @@ class Main:
         plugin_version = plugin_info["version"]
         if plugin_info["name"] in self.loaded_plugins:
             return 0
-        self.logger.notice(f"Getting plugin {name} infos...")
+        self.logger.notice(self.liblary("load_plugin").format(name))
         if plugin_info["api_version"] != version.podrum_api_version:
-            self.logger.warn(f"""Plugin {plugin_info["name"]} can't be loaded due to bad plugin API version. """)
-            self.logger.warn(f"""Plugin API version {plugin_info["api_version"]}""")
-            self.logger.warn(f"Required API version {version.podrum_api_version}")
+            self.logger.warn(self.liblary("wrong_api").format(name))
+            self.logger.warn(self.liblary("wrong_api2").format(plugin_info["api_version"]))
+            self.logger.warn(self.liblary("wrong_api3").format(version.podrum_api_version))
             return 1
         if plugin_info["name"] in self.server.managers.plugin_manager.plugins:
-            self.logger.info("Unloading stage 1/1 ...")
+            self.logger.info(self.liblary("unload_plugin").format(name))
             del self.server.managers.plugin_manager.plugins[plugin_info["name"]]
         # self.logger.info("Making new ZIP file")
         # shutil.make_archive(f"{name}_{plugin_version}","zip",path)
         # shutil.move(f"{name}_{plugin_version}.zip",f"plugins/{name}_{plugin_version}.zip")
         # self.logger.success("Successfully maked ZIP file")
-        self.logger.info("Server please load this plugin now...")
+
         #self.server.managers.plugin_manager.load(f"{self.server.get_root_path()}\\..\\plugins\\{name}_{plugin_version}.zip")
         spec = importlib.util.spec_from_file_location(f"{class_name}", f"{path}\{file_name}.py")
         prev_main_class = spec.loader.load_module(spec.name)
@@ -55,7 +71,7 @@ class Main:
         self.server.managers.plugin_manager.plugins[plugin_info["name"]].author = plugin_info["author"]
         if hasattr(main_class, "on_load"):
             self.server.managers.plugin_manager.plugins[plugin_info["name"]].on_load()
-        self.logger.success(f"""Thank you server for loading {plugin_info["name"]}""")
+        self.logger.success(self.liblary("success_loading").format(name))
         return 0
 
     def getAllFolders(self) -> None:
@@ -67,6 +83,6 @@ class Main:
                     if self.toxic_load(os.path.join(self.plugin_folder_path, dirs)) == 0:
                         howMuch = howMuch+1
         if howMuch != 0:
-            self.logger.info(f"Loaded {howMuch} plugins")
+            self.logger.info(self.liblary("loaded_x_plugins").format(howMuch))
         else:
             self.logger.info("There is no plugins to load")
