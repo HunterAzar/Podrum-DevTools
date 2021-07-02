af4c9d22cc0f97b8ff94da798fd335a9f415e79b
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-02 17:49:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ class Main:
         self.loaded_plugins: list = []
         
     def on_load(self):
-        self.server.logger.info("DevTools is on fire!!!")
+        self.logger: logger = self.server.logger
+        self.logger.info("DevTools is on fire!!!")
         self.getAllFolders()
 
     def on_unload(self):
@@ -28,20 +29,20 @@ class Main:
         plugin_version = plugin_info["version"]
         if plugin_info["name"] in self.loaded_plugins:
             return 0
-        self.server.logger.notice(f"Getting plugin {name} infos...")
+        self.logger.notice(f"Getting plugin {name} infos...")
         if plugin_info["api_version"] != version.podrum_api_version:
-            self.server.logger.warn(f"""Plugin {plugin_info["name"]} can't be loaded due to bad plugin API version. """)
-            self.server.logger.warn(f"""Plugin API version {plugin_info["api_version"]}""")
-            self.server.logger.warn(f"Required API version {version.podrum_api_version}")
+            self.logger.warn(f"""Plugin {plugin_info["name"]} can't be loaded due to bad plugin API version. """)
+            self.logger.warn(f"""Plugin API version {plugin_info["api_version"]}""")
+            self.logger.warn(f"Required API version {version.podrum_api_version}")
             return 1
         if plugin_info["name"] in self.server.managers.plugin_manager.plugins:
-            self.server.logger.info("Unloading stage 1/1 ...")
+            self.logger.info("Unloading stage 1/1 ...")
             del self.server.managers.plugin_manager.plugins[plugin_info["name"]]
         # self.logger.info("Making new ZIP file")
         # shutil.make_archive(f"{name}_{plugin_version}","zip",path)
         # shutil.move(f"{name}_{plugin_version}.zip",f"plugins/{name}_{plugin_version}.zip")
         # self.logger.success("Successfully maked ZIP file")
-        self.server.logger.info("Server please load this plugin now...")
+        self.logger.info("Server please load this plugin now...")
         #self.server.managers.plugin_manager.load(f"{self.server.get_root_path()}\\..\\plugins\\{name}_{plugin_version}.zip")
         spec = importlib.util.spec_from_file_location(f"{class_name}", f"{path}\{file_name}.py")
         prev_main_class = spec.loader.load_module(spec.name)
@@ -54,7 +55,7 @@ class Main:
         self.server.managers.plugin_manager.plugins[plugin_info["name"]].author = plugin_info["author"]
         if hasattr(main_class, "on_load"):
             self.server.managers.plugin_manager.plugins[plugin_info["name"]].on_load()
-        self.server.logger.success(f"""Thank you server for loading {plugin_info["name"]}""")
+        self.logger.success(f"""Thank you server for loading {plugin_info["name"]}""")
         return 0
 
     def getAllFolders(self) -> None:
@@ -62,10 +63,10 @@ class Main:
         for dirs in os.listdir(self.plugin_folder_path):
             if os.path.isdir(os.path.join(self.plugin_folder_path, dirs)):
                 if dirs != "DevTools":
-                    self.server.logger.info(f"Loading {os.path.join(self.plugin_folder_path, dirs)} plugin")
+                    self.logger.info(f"Loading {os.path.join(self.plugin_folder_path, dirs)} plugin")
                     if self.toxic_load(os.path.join(self.plugin_folder_path, dirs)) == 0:
                         howMuch = howMuch+1
         if howMuch != 0:
-            self.server.logger.info(f"Loaded {howMuch} plugins")
+            self.logger.info(f"Loaded {howMuch} plugins")
         else:
-            self.server.logger.info("There is no plugins to load")
+            self.logger.info("There is no plugins to load")
