f5fd69a0fd8325c3736b13f5ea58b557ba0c28b8
Tobiasz <0tobik09@gmail.com>
2021-11-20 17:48:50 +0100

    C:\Users\Tobiasz\Podrum\plugins\Podrum-DevTools\:art: Changed main class name for compatibility reasons

---
+++ devtools.py
@@ -0,0 +1,89 @@
+import importlib
+import importlib.util
+import json
+from podrum.version import version
+from podrum.console.logger import logger
+import os
+from commands.makeplugin import make_plugin
+from threading import Timer
+from typing import TYPE_CHECKING
+import shutil as zip
+if TYPE_CHECKING:
+    from podrum.server import server
+    from podrum.plugin_manager import plugin_manager
+
+
+class Tool:
+    def __init__(self) -> None:
+        self.plugin_folder_path: str = os.path.join(os.getcwd(), "plugins")
+        if not os.path.exists(f"{self.plugin_folder_path}/DevTools_Plugins"):
+            os.makedirs(f"{self.plugin_folder_path}/DevTools_Plugins")
+
+        self.loaded_plugins: dict = {}
+        self.before_msg = "[ DevTools ] >> "
+        self.server: 'server'
+        self.plugin_manager: 'plugin_manager'
+
+    def on_load(self) -> None:
+        self.plugin_manager = self.server.managers.plugin_manager
+        self.logger: logger = self.server.logger
+        self.logger.info("DevTools is working!!!")
+        self.server.managers.command_manager.register(make_plugin(self.server))
+        self.getAllFolders()
+
+    def on_unload(self) -> None:
+        self.logger.notice(self.before_msg+" Bye")
+
+    def toxic_load(self, path: str) -> int:
+        try:
+            plugin_info: dict = json.load(
+                open(os.path.join(path, "info.json"), 'r'))
+        except:
+            print("not a plugin")
+            return 1
+        name = plugin_info["name"]
+        if name in self.plugin_manager.plugins:
+            self.logger.info(f"Already loaded {name}")
+            return 1
+        print(name)
+        print(self.plugin_manager.plugins)
+        if(name=="DevTools"): return 1
+        plugin_version = plugin_info["version"]
+        if name in self.loaded_plugins:
+            return 1
+        self.logger.info("lol")
+        if plugin_info["api_version"] != version.podrum_api_version:
+            self.logger.warn("Wrong API!!!")
+            self.logger.warn(f"You are using {plugin_version}")
+            self.logger.warn(f"But we support {version.podrum_api_version}")
+            return 1
+        
+        
+        zip.make_archive(f"{plugin_info['name']}_{plugin_info['version']}","zip",path)
+
+        if(os.path.exists(f"{self.plugin_folder_path}\\DevTools_Plugins\\{plugin_info['name']}_{plugin_info['version']}.zip")):
+            os.remove(f"{self.plugin_folder_path}\\DevTools_Plugins\\{plugin_info['name']}_{plugin_info['version']}.zip")
+
+        zip.move(f"{plugin_info['name']}_{plugin_info['version']}.zip",
+                 f"{self.plugin_folder_path}\\DevTools_Plugins")
+        return 0
+
+    def getAllFolders(self) -> None:
+        howMuch = 0
+        print("6")
+        for dirs in os.listdir(self.plugin_folder_path):
+            print("5")
+            if os.path.isdir(os.path.join(self.plugin_folder_path, dirs)):
+                print("4")
+                if dirs != "Podrum-DevTools":
+                    print("2")
+                    print(dirs)
+                    if self.toxic_load(os.path.join(self.plugin_folder_path, dirs)) == 0:
+                        howMuch = howMuch+1
+                        print("1")
+                else:
+                    print("3")
+            print("7")
+        print("8")
+        if howMuch != 0:
+            self.logger.info(f"Loaded {howMuch} plugins")
--- utils/ToxicLoader.py
+++ utils/ToxicLoader.py
@@ -1,86 +1,101 @@
 from shutil import Error
 from typing import TYPE_CHECKING
+from time import sleep
 
-import os, json
+import os
+import json
 
 if TYPE_CHECKING:
+    from podrum.version import version
     from podrum.server import server
+
 
 class ToxicLoader:
 
+    """
+        ...
 
+        Attributes
+        ----------
+        input_folder : str
+            Path to the folder with the plugins to load.
+        output_folder : str
+            Path to the folder where the loaded plugins will be stored.
+        check_if_wants : bool
+            If True, the loaded plugins will be checked if they want to be loaded by DevTools.
+        server : 'server'
+            The server instance.
+
+    """
+
+    def __init__(self, input_folder: str, output_folder: str, check_if_wants: bool, server: 'server') -> None:
+        self.input_folder = input_folder
+        self.output_folder = output_folder
+        self.check_if_wants = check_if_wants
+        self.server = server
+        self.logger = server.logger
+        sleep(10)
+
+    def validate_plugin_json(self, plugin_json: dict) -> list[bool, str]:
         """
-            ...
+            Validates the plugin json.
 
-            Attributes
+            Parameters
             ----------
-            input_folder : str
-                Path to the folder with the plugins to load.
-            output_folder : str
-                Path to the folder where the loaded plugins will be stored.
-            check_if_wants : bool
-                If True, the loaded plugins will be checked if they want to be loaded by DevTools.
-            server : 'server'
-                The server instance.
-            
-        """    
-        def __init__(self, input_folder: str, output_folder: str, check_if_wants: bool, server: 'server') -> None:
-            self.input_folder = input_folder
-            self.output_folder = output_folder
-            self.check_if_wants = check_if_wants
-            self.server = server
+            plugin_json : dict
+                The plugin json.
 
-        def validate_plugin_json(self, plugin_json: dict) -> list[bool, str]:
-            """
-                Validates the plugin json.
-                
-                Parameters
-                ----------
-                plugin_json : dict
-                    The plugin json.
-                
-                Returns
-                -------
-                list[bool, str]
-                    [True, ""]
-                        if the plugin json is valid.
-                    [False, error_message: str]
-                        if the plugin json is not valid.
-            """
-            errors: list[str] = []
-            if not "name" in plugin_json:
-                errors.append("Missing plugin name")
-            if not "version" in plugin_json:
-                errors.append("Missing plugin version")
-            if not "api_version" in plugin_json:
-                errors.append("Missing plugin api_version")
-            if not "main" in plugin_json:
-                errors.append("Missing plugin main class")
-            
-            if len(errors) > 0:
-                return [False, "\n".join(errors)]
-            return [True, ""]
+            Returns
+            -------
+            list[bool, str]
+                [True, ""]
+                    if the plugin json is valid.
+                [False, error_message: str]
+                    if the plugin json is not valid.
+        """
+        errors: list[str] = []
+        if not "name" in plugin_json:
+            errors.append("Missing plugin name")
+        if not "version" in plugin_json:
+            errors.append("Missing plugin version")
+        if not "api_version" in plugin_json:
+            errors.append("Missing plugin api_version")
+        if plugin_json["api_version"] != version.podrum_api_version:
+            errors.append(
+                f"Plugin api_version is not the same as the server api_version!\nPlugin api_version: {plugin_json['api_version']}\nServer api_version: {version.podrum_api_version}"
+                )
+        if not "main" in plugin_json:
+            errors.append("Missing plugin main class")
 
-        def load_plugin_by_path(self, plugin_path: str) -> None:
-            """
-                Loads a plugin by its path.
-                
-                Parameters
-                ----------
-                plugin_path : str
-                    Path to the plugin to load.
-            """
-            #check if file info.json exists
-            if not os.path.exists(os.path.join(plugin_path, "info.json")):
-                FileNotFoundError(f"{plugin_path}/info.json not found!\nNOT VALID PLUGIN STRUCTURE!!!")
-                return #? xD
+        if len(errors) > 0:
+            return [False, "\n".join(errors)]
+        return [True, ""]
 
-            plugin_info: dict = json.load(
-                open(os.path.join(plugin_path, "info.json"), 'r')
-            )
+    def load_plugin(self, plugin_path: str) -> None:
+        """
+            Loads a plugin by its path.
 
-            if(not self.validate_plugin_json(plugin_info)[0]):
-                Error(f"{plugin_path}/info.json is not valid!\n{self.validate_plugin_json(plugin_info)[1]}")
-            
+            Parameters
+            ----------
+            plugin_path : str
+                Path to the plugin to load.
+        """
+        if not os.path.exists(os.path.join(plugin_path, "info.json")):
+            FileNotFoundError(
+                f"{plugin_path}/info.json not found!\nNOT VALID PLUGIN STRUCTURE!!!")
+            return  # ? xD
 
-            
+        plugin_info: dict = json.load(
+            open(os.path.join(plugin_path, "info.json"), 'r')
+        )
+
+        if(not self.validate_plugin_json(plugin_info)[0]):
+            Error(
+                f"{plugin_path}/info.json is not valid!\n{self.validate_plugin_json(plugin_info)[1]}")
+
+        name = plugin_info["name"]
+        if name in self.server.plugin_manager.plugins:
+            self.server.logger.info(f"Already loaded {name}")
+            return
+        main = plugin_info["main"]
+        plugin_version = plugin_info["version"]
